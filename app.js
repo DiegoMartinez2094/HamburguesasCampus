e9a4dc9fab5c42c5624127eaca5f2bcfffa0b945
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-09-04 09:47:18 -0500

    feat: :sparkles: ejemplo de versionRoutes
    se implementa un ejemplo para manejar las diferentes versiones

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 import express from "express";
 import versionRoutes from "express-routes-versioning";
 import { check } from "express-validator";
+
 import { limitGrt } from "./limit/config.js";
+import usuario from "./routers/V1/ejemplo.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +12,67 @@ const versionRoute = versionRoutes();
 
 app.use(express.json());
 const config = JSON.parse(process.env.MY_SERVER);
+
+app.use(
+  "/usuario",
+  [
+    check("id_usuario")
+      .notEmpty()
+      .withMessage("el id_usuario es obligatorio")
+      .custom((value) => /^\d+$/.test(value))
+      .withMessage("El id_usuario debe ser numérico sin letras")
+      .toInt(),
+
+    check("nombre")
+      .notEmpty()
+      .withMessage("el nombre es obligatorio")
+      .isString()
+      .withMessage("el nombre debe ser string"),
+
+    check("correo")
+      .notEmpty()
+      .withMessage("El correo es obligatorio")
+      .isString()
+      .withMessage("El correo debe ser un string")
+      .isEmail()
+      .withMessage("El correo debe ser una dirección de correo válida"),
+
+    check("contraseña")
+      .notEmpty()
+      .withMessage("La contraseña es obligatoria")
+      .custom((value) => /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/.test(value))
+      .withMessage(
+        "La contraseña debe contener al menos 8 caracteres, incluyendo números y letras"
+      ),
+
+    check("rol")
+      .notEmpty()
+      .withMessage("el rol es obligatorio")
+      .custom((value) =>
+        ["cliente", "repartidor"].includes(value.toLowerCase())
+      )
+      .withMessage(
+        "rol no válido debee ser alguno de estos: repartidor o cliente"
+      ),
+
+    check("telefono")
+      .notEmpty()
+      .withMessage("el telefono es obligatorio")
+      .isString()
+      .withMessage("el telefono debe ser string"),
+
+    check("direccion")
+      .notEmpty()
+      .withMessage("la direccion es obligatorio")
+      .isString()
+      .withMessage("la direccion debe ser string"),
+  ],
+
+  versionRoute({
+    "1.0.0": usuario,
+    "2.0.0": usuario2,
+  })
+);
 
 
 app.listen(config.port, config.hostname, () => {

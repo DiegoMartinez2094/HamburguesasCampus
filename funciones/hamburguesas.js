003742eb341f2ae21243df5ab0851d526b50f1d8
DiegoMartinez~ <diegofernandomartinezcalderon@gmail.com>
2023-09-06 18:22:24 -0500

    feat: :sparkles: se implementa un nuevo router hamburguesa
    este  router filtra las hamburguesas preparadas por el chefB

--- funciones/hamburguesas.js
+++ funciones/hamburguesas.js
@@ -7,7 +7,20 @@ export async function hamburguesaVegetariana() {
       const result = await ingredients.find({ "categoria":{ $eq:"vegetariana"}}).toArray();
       return result;
     } catch (error) {
-      console.error("Error al obtener los ingrediente:", error);
+      console.error("Error al obtener las hamburguesa:", error);
       throw error; 
     }
   }
+
+  export async function hamburguesaXchefB() {
+    try {
+      const db = await con();
+      const ingredients = db.collection("hamburguesa");
+      const result = await ingredients.find({"preparadaPor":{ $eq: "chefB"} }).toArray();
+      return result;
+    } catch (error) {
+      console.error("Error al obtener las hamburguesa:", error);
+      throw error; 
+    }
+  }
+
--- routers/V1/hamburguesa.js
+++ routers/V1/hamburguesa.js
@@ -2,6 +2,8 @@
 import { Router } from "express";
 import { limitGrt } from "../../limit/config.js";
 import {hamburguesaVegetariana} from "../../funciones/hamburguesas.js"
+import { hamburguesaXchefB } from "../../funciones/hamburguesas.js";
+import { hamburguesaPanIntegral } from "../../funciones/hamburguesas.js";
 
 const hamburguesa = Router();
 
@@ -15,6 +17,19 @@ hamburguesa.get("/hamburguesaVegetariana", limitGrt(), async (req, res) => {
       res.status(500).send("Error interno del servidor");
     }
   });
+
+  hamburguesa.get("/hamburguesaXchefB", limitGrt(), async (req, res) => {
+    if (!req.rateLimit) return;
+    console.log(req.rateLimit);
+    try {
+      const hambur = await hamburguesaXchefB();
+      res.send(hambur);
+    } catch (error) {
+      res.status(500).send("Error interno del servidor");
+    }
+  });
+
+  
 
 export default hamburguesa;
 
